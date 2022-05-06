00f78c1f867354d19e71cc3d8139f387a0853174
Alysson Iwamoto paladini <101071916+alyssonpaladini@users.noreply.github.com>
2022-05-06 10:39:05 -0300

    Update
    Day 4 challenge.
    HTML changed, we add more sections and color slider.
    CSS changed, we add more styles for the new sections.
    JS changed, we add some functions.

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
+let value=0;
+let doc = document.documentElement;
+
+let slider = document.querySelector("#cor");
+slider.oninput = function() {
+    document.documentElement.style.setProperty("--hue", this.value);
+  }
+
+
+window.addEventListener("scroll",onScroll);
+window.addEventListener("scroll",function(){
+    return value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight));
+});
+
+onScroll();
+
 function onScroll(){
+    showNavOnScroll();
+    showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll(){
     if(scrollY>0){
         navigation.classList.add("scroll");    
     }else{
         navigation.classList.remove("scroll");
+    }
+}
+
+function showBackToTopButtonOnScroll(){
+    if(scrollY>200 && value<95){
+        backToTopButton.classList.add("show");
+        backToTopButton.classList.remove("show-contrast");    
+    }else{
+        backToTopButton.classList.remove("show");
+    }
+
+    if(value>=95){
+        backToTopButton.classList.remove("show");
+        backToTopButton.classList.add("show-contrast");    
+    }else{
+        backToTopButton.classList.remove("show-contrast");
     }
 }
 
